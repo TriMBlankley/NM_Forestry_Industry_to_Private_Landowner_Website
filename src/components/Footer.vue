a7aed412eb59ae7b8f1018d5e6a1555c6bad93a1
TriMBlankley <triblankley@gmail.com>
2025-02-13 11:39:52 -0700

    Created Footer.vue and changed main and base.css to get our color pallete in the site

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="nmf-footer">
+        <p> Some Text!</p>
+    </div>
+</template>
+
+<style scoped src="@/assets/base.css">
+.nmf-footer {
+    color: var(--nmf-t-bkContrast)
+    background-color: var(--nmf-bk-contrast);
+
+    display: flex;
+    justify-content: space-around;
+
+    position: absolute;
+    transform: translateY(-80vh);
+    width: 100vw;
+
+    top: 0%;
+}
+</style>
+
