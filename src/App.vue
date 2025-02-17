3a44293e4af30e8ba5b4e10a63924195d9d5a7cd
TriMBlankley <triblankley@gmail.com>
2025-02-17 15:29:57 -0700

    Removed HelloWorld.vue, TheWelcome.vue, and WelcomeItem.vue, and updated the router to point directly to mapRouter

--- src/App.vue
+++ src/App.vue
@@ -9,25 +9,9 @@ import MapFilterView from './views/MapFilterView.vue'
   <body>
     <MapFilterView />
   </body>
-  
-  
 </template>
 
 <style scoped>
-
-.map-holder {
-  position: absolute;
-  top: 50%;
-  left: 5%;
-  width: 40%;
-  height: 70%;
-  transform: translateY(-50%);
-}
-
-.map{
-  width: 100%;
-  height: 100%;
-}
 
 
 @media (min-width: 1024px) {
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MapFilterView from '@/views/MapFilterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: MapFilterView,
     },
     {
       path: '/about',
